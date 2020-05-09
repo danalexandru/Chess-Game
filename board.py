66d5d476b4646818b222a6d68bb92f2ca3c4fe75
danalexandru <danb.alexandru@gmail.com>
2020-05-09 12:18:53 +0300

    [fix]   - incorrect placement of the chess pieces (they were backwards :P)   - added chessboard_wallpaper_3.jpg with the correct chess board orientation   - changed the init function from board.py in order to place the Queens and Kings in the correct positions   - changed the 'chessboard.wallpaper.image' value from config.properties in order to use the new chessboard_wallpaper_3.jpg

--- board.py
+++ board.py
@@ -44,8 +44,8 @@ class Board:
             self.board_inst[0][0] = Rook(0, 0, 'black')
             self.board_inst[0][1] = Knight(0, 1, 'black')
             self.board_inst[0][2] = Bishop(0, 2, 'black')
-            self.board_inst[0][3] = King(0, 3, 'black')
-            self.board_inst[0][4] = Queen(0, 4, 'black')
+            self.board_inst[0][3] = Queen(0, 3, 'black')
+            self.board_inst[0][4] = King(0, 4, 'black')
             self.board_inst[0][5] = Bishop(0, 5, 'black')
             self.board_inst[0][6] = Knight(0, 6, 'black')
             self.board_inst[0][7] = Rook(0, 7, 'black')
@@ -53,8 +53,8 @@ class Board:
             self.board_inst[7][0] = Rook(7, 0, 'white')
             self.board_inst[7][1] = Knight(7, 1, 'white')
             self.board_inst[7][2] = Bishop(7, 2, 'white')
-            self.board_inst[7][3] = King(7, 3, 'white')
-            self.board_inst[7][4] = Queen(7, 4, 'white')
+            self.board_inst[7][3] = Queen(7, 3, 'white')
+            self.board_inst[7][4] = King(7, 4, 'white')
             self.board_inst[7][5] = Bishop(7, 5, 'white')
             self.board_inst[7][6] = Knight(7, 6, 'white')
             self.board_inst[7][7] = Rook(7, 7, 'white')
@@ -63,8 +63,8 @@ class Board:
                 self.board_inst[1][i] = Pawn(1, i, 'black')
                 self.board_inst[6][i] = Pawn(6, i, 'white')
 
-            king_positions_handler.set_value((0, 3), 'black')
-            king_positions_handler.set_value((7, 3), 'white')
+            king_positions_handler.set_value((0, 4), 'black')
+            king_positions_handler.set_value((7, 4), 'white')
 
             return
         except Exception as error_message:
